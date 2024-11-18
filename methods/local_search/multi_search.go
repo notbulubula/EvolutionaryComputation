e843bd7137b238e5af1df0ba2316cec81b3c6320
SamuelJanas <sneakyinsect@gmail.com>
2024-11-18 17:49:33 +0100

    baseline of Assignment 6

--- methods/local_search/multi_search.go
+++ methods/local_search/multi_search.go
@@ -1,6 +1,9 @@
 package local_search
 
-import "evolutionary_computation/utils"
+import (
+    "time"
+    "evolutionary_computation/utils"
+)
 
 func MultiLocalSearch(costMatrix [][]int, pointless_value int) []int {
 	var bestFitness int
@@ -19,4 +22,41 @@ func MultiLocalSearch(costMatrix [][]int, pointless_value int) []int {
 	}
 
 	return bestSolution
+}
+
+
+func IterativeLocalSearch(costMatrix [][]int, pointless_value int) []int {
+    var bestFitness int
+    var bestSolution []int
+    var callCount int
+    var solution []int
+
+    startTime := time.Now()
+
+    //TODO: change the time to average from MultiLocalSearch
+    for time.Since(startTime) < 30*time.Second {
+        startNode := callCount % len(costMatrix)
+
+        if callCount == 0 {
+            solution = RandomSteepestIntraEdge(costMatrix, startNode)
+        } else {
+            bestSolutionCopy := make([]int, len(bestSolution))
+            copy(bestSolutionCopy, bestSolution)
+            //TODO add permutation of bestSolutionCopy
+
+
+            solution = SteepestIntraEdgeFromSolution(bestSolutionCopy, costMatrix, startNode)
+        }
+
+        callCount++
+        fitness := utils.Fitness(solution, costMatrix)
+
+        if callCount == 1 || fitness < bestFitness {
+            bestFitness = fitness
+            bestSolution = solution
+        }
+    }
+    // TODO: add callCount to results dict
+    println("Number of RandomSteepestIntraEdge calls:", callCount)
+    return bestSolution
 }
